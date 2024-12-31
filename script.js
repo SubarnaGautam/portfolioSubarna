7e87cf244cac95a5b1637634209ade1486500fe1
suborn <whatwevedecided@gmail.com>
2024-12-31 19:47:06 +1100

    suttle change on buttons

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const experienceBtn = document.querySelector(".experience-btn")
+const nextarrow = document.querySelector(".arrow-next")
+const prevarrow = document.querySelector(".arrow-prev")
+const body1el = document.querySelector(".body1")
+
 
 experienceBtn.addEventListener("click", function(){
     window.location.href = "experience.html"
 })
+nextarrow.addEventListener("click", function(){
+    body1el.style.background = "url('sources/IMG_2684.jpg')"
+})
+prevarrow.addEventListener("click", function(){
+    body1el.style.background = "url('sources/IMG_9167.png')"
+})
+
